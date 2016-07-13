c89c8f7b074cb9ca6673c5640eb189596bfb0bae
JXSTY <596313217@qq.com>
2016-07-13 11:56:50 +0800

    首页基本完成

--- src/js/page/index/index.js
+++ src/js/page/index/index.js
@@ -3,16 +3,16 @@
 
         $(window).scroll(function() {
             var domTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var $level2 = $('#level2');
             console.log("dom卷起距离=" + domTop);
             domTop >= 550? headerFixed():headerUnFixed();
+            $level2.css('padding-top',130);
         });
-
     });
 
     function headerFixed(){
         $('#header').addClass('fixed');
     }
-
     function headerUnFixed(){
         $('#header').removeClass('fixed');
     }
